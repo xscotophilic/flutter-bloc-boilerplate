e8623576328462ecd27a1fcbe8a7b736c5423cd5
Chintan <chintan.170410116089@gmail.com>
2022-10-24 15:22:23 +0530

    removed: unwanted imports from theme_api.dart

--- packages/theme_api/lib/src/theme_api.dart
+++ packages/theme_api/lib/src/theme_api.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:meta/meta.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeApi {
